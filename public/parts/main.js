464dabb91f8939bac7afc123047adf96806a0ef6
tannerkandor <tanner.kandor@gmail.com>
2023-08-16 11:55:39 -0500

    3100 bezel b grade a grade

--- public/parts/main.js
+++ public/parts/main.js
@@ -47,6 +47,18 @@ function backcover() {
 		  navigator.clipboard.writeText(copyText.value);
 
 		}
+		function bezel2() {
+			// Get the text field
+			var copyText = document.getElementById("bezel2");
+  
+			// Select the text field
+			copyText.select();
+			copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+			 // Copy the text inside the text field
+			navigator.clipboard.writeText(copyText.value);
+  
+		  }
 		function bottomCover() {
 		  // Get the text field
 		  var copyText = document.getElementById("bottomCover");
